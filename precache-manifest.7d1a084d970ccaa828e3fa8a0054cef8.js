0a02c9889b35ec037332afd1ce5ff493f1fafae4
Cherrie Gabriel <cmgabriel@myseneca.ca>
2019-06-12 06:05:14 -0400

    Updates

---
+++ precache-manifest.7d1a084d970ccaa828e3fa8a0054cef8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cfcf3a19340d7a3652cba7627e7bacc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8b263c3211366df34b2",
+    "url": "/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "9a5c0cc5b7b5942488d9",
+    "url": "/static/js/2.56cb80cd.chunk.js"
+  },
+  {
+    "revision": "d8b263c3211366df34b2",
+    "url": "/static/js/main.0259f105.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
